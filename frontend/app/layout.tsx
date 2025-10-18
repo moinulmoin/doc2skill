c3ae87890ccf8f2ca98d6577655aeba75909dff5
Moinul Moin <moinulmoin101@gmail.com>
2025-10-18 21:46:37 +0600

    feat: add Umami web analytics with Next.js Script component and add comments for analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Analytics } from "@/components/analytics";
 
@@ -29,8 +30,16 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        {/* not working */}
         <Analytics/>
       </body>
+      {/* Umami web analytics for visitor statistics */}
+      <Script
+        defer
+        src="https://umami.moinulmoin.com/script.js"
+        data-website-id="d8600839-5ab5-4257-a2c5-71225affbb04"
+        strategy="afterInteractive"
+      />
     </html>
   );
 }
